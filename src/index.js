7ad1d9adca36d1247767d787f0a42a86c4d56a4a
Apple <apple@MacBook-Air-Apple.local>
2020-09-09 17:17:39 +0300

    pulling PostData though props in index.js/problems in logo

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './NewProject/App';
 import './index.css';
+import logo from './NewProject/Content/Profile/Posts/Post/logo512.png';
 
+let PostData = [
+  { id: 1, countLike: 14, img: { logo }, post: 'LKIOO' },
+  { id: 2, countLike: 21, img: { logo }, post: 'Helloo' },
+  { id: 3, countLike: 11, img: { logo }, post: 'Hoy Hey' },
+  { id: 4, countLike: 14, img: { logo }, post: 'LKIOOa' },
+  { id: 5, countLike: 14, img: { logo }, post: 'LKIOOa' },
+  { id: 6, countLike: 14, img: { logo }, post: 'LKIOOra' },
+  { id: 1, countLike: 14, img: { logo }, post: 'LKIOO' },
+  { id: 2, countLike: 21, img: { logo }, post: 'Helloo' },
+  { id: 3, countLike: 11, img: { logo }, post: 'Hoy Hey' },
+  { id: 4, countLike: 14, img: { logo }, post: 'LKIOOa' },
+  { id: 5, countLike: 14, img: { logo }, post: 'LKIOOa' },
+  { id: 6, countLike: 14, img: { logo }, post: 'LKIOOra' }
+];
 
 
 ReactDOM.render(  
   <React.StrictMode>
-    <App />
+    <App PostData={PostData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
