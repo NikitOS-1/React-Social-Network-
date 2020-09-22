67dfb1bb877896fe63c820456b0387e2ff9064e1
Apple <apple@MacBook-Air-Apple.local>
2020-09-22 21:48:12 +0300

    update message flex

--- src/NewProject/Content/Profile/Posts/Post/Post.jsx
+++ src/NewProject/Content/Profile/Posts/Post/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from './Post.module.css';
+import ava from './logo512.png'
 
 const PostIng = (props) => {
     return (
         <div className={s.Post}>
             <div className={s.fullPost}>
-                <img src={props.ava} alt='Your Ava'/>
+                <img src={ava} alt='Your Ava'/>
                 <p className={s.postText}>{props.post}</p>
                 <p className={s.id}>{props.id}</p>
                 <p className={s.countLike}>Likes: {props.countLike}</p>
@@ -16,7 +17,7 @@ const PostIng = (props) => {
 
 
 const Post = (props) => {
-    let postElement = props.PostData.map(p => <PostIng id={p.id} countLike={p.countLike} post={p.post} ava={props.ava}/>);
+    let postElement = props.PostData.map(p => <PostIng id={p.id} countLike={p.countLike} post={p.post} ava={ava}/>);
     return (
         <div>
             {postElement}
