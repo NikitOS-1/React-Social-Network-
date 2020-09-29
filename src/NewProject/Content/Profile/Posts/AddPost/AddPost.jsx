1915b55433becadf576e611c51064299aa26e90e
Apple <apple@MacBook-Air-Apple.local>
2020-09-29 16:52:55 +0300

    create ref link/

--- src/NewProject/Content/Profile/Posts/AddPost/AddPost.jsx
+++ src/NewProject/Content/Profile/Posts/AddPost/AddPost.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import s from './AddPost.module.css'
 
 const AddPost = () => {
+    let text_AddPost = React.createRef();
+    let changeText_AddPost = () => {
+        let text = text_AddPost.current.value;
+    }
+    let click_AddPost = () => {
+        
+    }
     return (
         <div className={s.AddPost}>
-            <textarea></textarea>
-            <button>Add Post</button>
+            <textarea onChange={changeText_AddPost} ref={text_AddPost} />
+            <button onClick={click_AddPost} >Add Post</button>
         </div>
     )
 };
